3e5f26ce1eca6c1d8a0980850d88a4930a1d8d70
vbasilioo <vbasilio2019@gmail.com>
2025-05-26 21:28:12 -0300

    chore(config): added next config parameters

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        NEXT_AUTH_SECRET: process.env.NEXT_AUTH_SECRET,
+    },
 };
 
 export default nextConfig;
--- src/app/api/client.ts
+++ src/app/api/client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.API_URL || 'http://localhost:3000';
+const BASE_URL = process.env.API_URL;
 
 interface ApiErrorData {
   message?: string;
